1d06c8285d62b55de9c3e4229f361f3c10bf11fb
jdwxp <609391330@qq.com>
2017-08-21 19:26:30 +0800

    Revert "Revert "test""
    This reverts commit 30c38063a0cc79bb1fa8697bcc529faa2597c569.

--- src/com/changmi/model/User.ts
+++ src/com/changmi/model/User.ts
@@ -1,10 +1,6 @@
 class User extends eui.Component{
-	public constructor(userName:string,goldNum:string){//,headImgData:egret.BitmapData) {
+	public constructor(){
 		super();
-		if(!userName){
-			this._hasUser = false;
-		}
-		this.createUserSource(userName,goldNum);//,headImgData);
 	}
 
 	private _hasUser:boolean;		//判断是否有用户在这位置上
@@ -37,9 +33,9 @@ class User extends eui.Component{
 
 	public set userName(userName:string){
 		this._userName = userName;
-		this.userNameLabel.text = userName;
-		console.log(userName);
+		console.log(this.userNameLabel.text);
 		
+		this.userNameLabel.text = userName;
 	}
 
 	public set goldNum(goldNum:string){
