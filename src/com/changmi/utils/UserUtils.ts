f2747a3ae84489a143cd28019b078d80dc8b83aa
jdwxp <609391330@qq.com>
2017-08-31 19:04:30 +0800

    排位置UserUtils

--- src/com/changmi/utils/UserUtils.ts
+++ src/com/changmi/utils/UserUtils.ts
@@ -1,7 +1,46 @@
 class UserUtils {
-	
+	private static _instance:UserUtils
+    /**存取所有桌上玩家信息 */
+    private userPools:Array<User>=[];
+	private ownUser:User;
+	public static getInstance(){
+    if(!this._instance){
+        this._instance = new UserUtils();
+    }
+    return this._instance;
+	}
+	public initUsers(users:Array<User>):void{
+		this.userPools=users;
+	}
+	public initUser(user:User):void{
+		this.userPools.push(user);
+	}
+	public getUsers():Array<User>{
+		return this.userPools;
+	}
+	public getUserFromIndex(index:number):User{
 
-	public constructor() {
-		
+		return this.userPools[index];
+	}
+	public saveOwnUser(user:User){
+		this.ownUser=user;
+	}
+	public sortUsersSeats(){
+		let ownIndex:number;
+		if(this.userPools.length!=0&&this.ownUser){
+			for(var i=0;i<this.userPools.length;i++){
+				if(this.userPools[i].userName==this.ownUser.userName){
+					this.userPools[i].seat=4;
+					ownIndex=i+1;
+					break;
+				}
+			}
+			for(var i=0;i<this.userPools.length;i++){
+				if(this.userPools[i].seat!=4){
+					this.userPools[i].seat=(i+1+Math.abs(4-ownIndex))%7
+				}
+				
+			}
+		}
 	}
 }
