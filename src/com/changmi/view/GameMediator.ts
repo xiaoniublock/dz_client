805fbdf39f7378e901bd00bc1983ca8b350770cf
jdwxp <609391330@qq.com>
2017-10-09 19:11:32 +0800

    reset清空

--- src/com/changmi/view/GameMediator.ts
+++ src/com/changmi/view/GameMediator.ts
@@ -179,6 +179,7 @@ module game {
                     this.gameScreen.hideOwnCards();
                     this.gameScreen.hideOtherCardsAndResetName();
                     this.gameScreen.hidePublicCard();
+                    CachePool.clearAll();
                     break;
                 }
             }
